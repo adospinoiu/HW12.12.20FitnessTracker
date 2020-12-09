98ab66cc58442202e7984d3cc3fabb3061a9f3ad
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2020-12-08 22:14:57 -0700

    worked on the apiRoutes and got the get, post, put to work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,16 @@ const Workout = require("../models/Workout.js");
 
 module.exports = function (app) {
 
+    app.get("/api/workouts", (req, res) => {
+        Workout.find({})
+          .then(dbUser => {
+            res.json(dbUser);
+          })
+          .catch(err => {
+            res.json(err);
+          });
+      });
+
     app.post("/api/workouts", ({ body }, res) => {
         Workout.create(body)
             .then(dbUser => {
@@ -13,14 +23,15 @@ module.exports = function (app) {
             });
     });
 
-    app.put("/api/workout/:id", ({ body }, res) => {
-        Workout.create(body)
-        .then(dbUser => {
-            res.json(dbUser);
-        })
-        .catch(err => {
-            res.json(err);
-        });
+    app.put("/api/workouts/:id", ({ body, params }, res) => {
+        Workout.findByIdAndUpdate(params.id, { $push: { exercises: body }})
+            .then(dbUser => {
+                res.json(dbUser);
+            })
+            .catch(err => {
+                res.json(err);
+                console.log(err);
+            });
     });
 
 
